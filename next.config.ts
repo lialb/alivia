c837b8a94391af64c92e63263d1fe22f54805abe
lialb <albert.yiliang@gmail.com>
2025-04-18 22:42:45 -0700

    fix script

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/alivia",
+  assetPrefix: "/alivia/",
 };
 
 export default nextConfig;
